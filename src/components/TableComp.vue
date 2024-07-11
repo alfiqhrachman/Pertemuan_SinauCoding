2c10d3af3ab20d2d4d0477c6b99d3e8bf43b9819
alfiqhrachman <113083141+alfiqhrachman@users.noreply.github.com>
2024-07-11 22:55:22 +0700

    pertemuan 8

---
+++ src/components/TableComp.vue
@@ -0,0 +1,35 @@
+<script setup>
+defineProps({
+  objTable: {
+    type: Object,
+    default: {},
+    require: false,
+  },
+});
+
+const emit = defineEmits(["showData"]);
+</script>
+
+<template>
+  <table border="1">
+    <thead>
+      <tr>
+        <th v-for="(data, index) in objTable.header" :key="index">
+          {{ data }}
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+        <tr v-for="data in objTable.data" :key="data.id">
+            <td> {{ data.id }}</td>
+            <td> {{ data.nama }}</td>
+            <td> {{ data.position }}</td>
+            <td> {{ data.no_telp }}</td>
+            <td> {{ data.email }}</td>
+            <td>
+                <button type="Sumbit" @click="emit('showData', data)">Show Data</button>
+            </td>
+        </tr>
+    </tbody>
+  </table>
+</template>
